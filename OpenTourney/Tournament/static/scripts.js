157e456525bc7a1169d8226df649d5bcc56719b5
20dshakespear <20dshakespear@gmail.com>
2023-04-09 20:00:07 -0600

    Added animations for going between pages. Fixed mobile navbar issue

---
+++ OpenTourney/Tournament/static/scripts.js
@@ -0,0 +1,23 @@
+// JavaScript code for page switching animation
+$(document).on('click', 'a', function(event) {
+    event.preventDefault();
+    var url = $(this).attr('href');
+    var currentUrl = window.location.href.replace(location.origin, '');
+    if(currentUrl !== url) {
+        $('#animation-page').addClass('out');
+        $.ajax({
+            url: url,
+            success: function(data) {
+                var newContent = $(data).filter('#animation-page').html();
+                $('#animation-page').removeClass('out').addClass('in');
+                setTimeout(function() {
+                    $('#animation-page').removeClass('in').addClass('show');
+                    $('#animation-page').html(newContent);
+                    setTimeout(function() {
+                        window.location.href = url;
+                    }, 500);
+                }, 500);
+            }
+        });
+    }
+});
